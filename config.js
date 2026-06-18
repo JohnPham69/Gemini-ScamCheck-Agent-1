f6efdf9724275b8e6d67a74192acf4481ea05e94
dangja2012-lang <dangja2012@gmail.com>
2026-06-18 19:47:16 +0700

    Change GEMINI_API_KEY in config.js
    Updated GEMINI_API_KEY to a new value.

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-const GEMINI_API_KEY = "AIzaSyDfmfKAVeXVOTiT_94CQbAbWCS5L4Wcmo0";
+const GEMINI_API_KEY = "AQ.Ab8RN6LMkcZTWpkJAuNwGzkxLWjMsoxE2PpDGyPAut6EXIenfA";
 const GEMINI_MODEL = "gemini-2.5-flash";
